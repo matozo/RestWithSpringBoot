3eae56b88f0518f4427d1cbb3bb6ca37d9a5369b
Anderson Matoso <andersonmatoso@gmail.com>
2020-01-13 23:02:47 -0300

    Aula 01

---
+++ Rest-with-spring-boot/src/test/java/br/com/erudio/Rest/with/spring/boot/RestWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.Rest.with.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
